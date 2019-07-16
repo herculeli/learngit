72ccea13f70ffd42ce82e16ea0a29f2e8397649c
herculeli <herculeli@126.com>
2019-07-16 15:16:34 +0800

    two rows

--- liblinearsvm.py
+++ liblinearsvm.py
@@ -10,8 +10,8 @@ from liblinearutil import *
 cc = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-def calCorrcoef(x,y):
-    x=x.reshape((1,x.size))
+#def calCorrcoef(x,y):
+#    x=x.reshape((1,x.size))
     y=y.reshape((1,y.size))
     m=np.concatenate((x,y),axis=0)
     res=np.corrcoef(m)
